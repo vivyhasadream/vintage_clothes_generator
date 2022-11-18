077c66b43cc77931337a7c7c1e232625a742510b
vivyhasadream <vivyhasadream@gmail.com>
2022-11-18 09:29:53 -0500

    organized folders, moved away unrelated notebooks, ignore testing files

--- functions.py
+++
@@ -1,9 +0,0 @@
-def TXTello():
-    print('Hello World!')
-
-import time
-
-def convert(seconds):
-    min, sec = divmod(seconds, 60)
-    hour, min = divmod(min, 60)
-    return '%d:%02d:%02d' % (hour, min, sec)
